fdef3abb3db2bc6937fd7b22ed63fd409ddfab28
Tiago Rodrigues <tiagorodrigues1590@hotmail.com>
2026-06-07 19:41:03 +0100

    Initial benchmarks placeholder files

---
+++ benchmarks/stack/bench_stack.c
@@ -0,0 +1,17 @@
+/*******************************************************************************************************
+* NAME: bench_stack.c
+*
+* PURPOSE: Benchmarks for the stack data structure
+*          Placeholder - to be filled in when benchmarks are implemented
+*
+* GLOBAL VARIABLES: None
+*
+* DEVELOPMENT HISTORY:
+*
+* Date          Author          Change Id       Release         Description Of Change
+* ----------    --------------- ---------       -------         -----------------------------------
+* 07-06-2026    Tiago Rodrigues                       1         File preparation
+*
+*******************************************************************************************************/
+
+/* placeholder */
