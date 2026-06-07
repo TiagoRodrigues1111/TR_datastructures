c6e763ea25c79024f077fdbdc4d58755bc25f552
Tiago Rodrigues <tiagorodrigues1590@hotmail.com>
2026-06-07 19:44:18 +0100

    Initial Fuzz placeholder files

---
+++ fuzz/stack/fuzz_stack.c
@@ -0,0 +1,17 @@
+/*******************************************************************************************************
+* NAME: fuzz_stack.c
+*
+* PURPOSE: Fuzz target for the stack data structure
+*          Placeholder - to be filled in when fuzz testing is implemented
+*
+* GLOBAL VARIABLES: None
+*
+* DEVELOPMENT HISTORY:
+*
+* Date          Author          Change Id       Release         Description Of Change
+* ----------    --------------- ---------       -------         -----------------------------------
+* 07-06-2026    Tiago Rodrigues                       1         File preparation
+*
+*******************************************************************************************************/
+
+/* placeholder */
